aacdbb8e3eaf663dc58bef8a0f97c7bae04619a4
pkduy1209 <pkduy1209@gmail.com>
2018-03-12 18:26:17 +0700

    laravel

---
+++ packages/foostart/sample/src/SampleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Foostart\Sample;
+
+use Illuminate\Support\ServiceProvider;
+
+class SampleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/views', 'sample');
+        $this->publishes([__DIR__.'/public' => public_path('foostart'),], 'public');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__ . '/routes.php';
+        $this->app->make('Foostart\Sample\SampleController');
+    }
+}
---
+++ packages/foostart/sample/src/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    
+    <link href="{{asset("foostart\css\app.css")}}" rel="stylesheet" type="text/css"/> 
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title"><?php echo $name ?></div>
+                <div class="des">This is sample packages</div>
+            </div>
+        </div>
+    </body>
+</html>
